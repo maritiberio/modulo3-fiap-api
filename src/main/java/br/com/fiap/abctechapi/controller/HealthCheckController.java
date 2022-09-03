51d40a1093cf95a4a2d876fea6b1323a1a8b4437
Mariana Marques Tiberio <marianatiberio@gmail.com>
2022-09-03 12:45:18 -0300

    health check controller com versao e nome

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -5,12 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
 
-    @GetMapping()
-    public ResponseEntity<Object> status(){
-        return ResponseEntity.ok().build();
+    @GetMapping("version")
+    public ResponseEntity<String> status() throws IOException {
+
+        Properties prop = new Properties();
+        InputStream inStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+        prop.load(inStream);
+
+        return ResponseEntity.ok(prop.getProperty("build.name") + " - " + prop.getProperty("build.version"));
     }
 }
