74013dcf870c61d4f8d88fc9abfa581d2c9975ca
Mariana Marques Tiberio <marianatiberio@gmail.com>
2022-10-15 12:34:51 -0300

    inserção de mais assistencias

--- src/main/resources/db/migration/V1__assists.sql
+++ src/main/resources/db/migration/V1__assists.sql
@@ -9,6 +9,10 @@ create table assistances
 INSERT INTO assistances (name, description) VALUES ('Troca de aparelho', 'Troca de aparelho decodificador de sinal');
 INSERT INTO assistances (name, description) VALUES ('Troca de cabo interno', 'Troca de cabo interno');
 INSERT INTO assistances (name, description) VALUES ('Troca de fiação interna', 'Substituição de fiação interna da residência');
+INSERT INTO assistances (name, description) VALUES ('Troca de bateria', 'Substituição de bateria de notebook');
+INSERT INTO assistances (name, description) VALUES ('Troca de tela', 'Substituição de tela de notebook');
 INSERT INTO assistances (name, description) VALUES ('Manutenção em fogão', 'Reparo sem necessidade de compra de peças');
 INSERT INTO assistances (name, description) VALUES ('Manutenção em geladeira', 'Reparo sem necessidade de compra de peças');
 INSERT INTO assistances (name, description) VALUES ('Manutenção em máquina de lavar', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Manutenção de secadora de roupa', 'Reparo sem necessidade de compra de peças');
+INSERT INTO assistances (name, description) VALUES ('Manutenção de ar condicionado', 'Reparo sem necessidade de compra de peças');
